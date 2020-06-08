97032b7aef4744c83fcf38a69730e9d379b118d7
DucMystery <58973644+DucMystery@users.noreply.github.com>
2020-06-08 21:31:41 +0700

    Them phan Account

--- src/CodeGym/service/account/Account.java
+++ src/CodeGym/service/account/Account.java
@@ -1,4 +1,13 @@
 package CodeGym.service.account;
 
 public class Account {
+    private String username;
+    private String password;
+
+    private Account(){}
+
+    public Account(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
 }
